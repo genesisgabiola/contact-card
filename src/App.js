647f77941b1e7251a5bc2734b0015cd9a4931d85
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-05-28 01:40:08 +0800

    Update App.js for learning basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const message = "This is my first variable rendered in JSX!";
+
+  const handleClick = () => {
+    alert("you clicked the message!");
+  }
+
+  return(
+    <div>
+      <h1>Hello React World</h1>
+      <h2 onClick={handleClick}>{message}</h2>
     </div>
   );
 }
